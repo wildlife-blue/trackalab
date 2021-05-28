647197149e57194ca7d544b51d3ae2e1196631ce
Nathaniel Brough <nathaniel.brough@gmail.com>
2021-05-28 19:49:53 +0800

    tr_spi: Completes spi interface
    Makes SPI interface non-copyable and adds Read/Write methods.

--- tr_spi/public/tr_spi/spi.h
+++ tr_spi/public/tr_spi/spi.h
@@ -1,13 +1,17 @@
 #pragma once
 #include <span>
 
+#include "pw_bytes/span.h"
+#include "pw_result/result.h"
 #include "pw_status/status.h"
+#include "tr_utils/type_attributes.h"
 
 namespace tr {
 namespace spi {
-class SpiInterface {
+class SpiInterface : public tr::utils::NonCopyable {
  public:
-  virtual pw::Status Write(std::span<const std::byte> buffer) = 0;
+  virtual pw::Status Write(pw::ConstByteSpan buffer) = 0;
+  virtual pw::Result<pw::ConstByteSpan> Read(pw::ByteSpan buffer) = 0;
 };
 }  // namespace spi
 }  // namespace tr
